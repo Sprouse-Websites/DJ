d6c354129f36f26592b3418d217ecb6cb213180f
Joel Sprouse <joel.sprouse11@gmail.com>
2020-04-14 17:31:52 +0100

    RCM
    This is the files needed for the right click menu

---
+++ rcm.php
@@ -0,0 +1,22 @@
+<!-- Right Click Menu -->
+<div id="rcm">
+	<a href="javascript:goBack()">
+		<i class="fas fa-arrow-left"></i> Back <span>Ctrl + ?!</span>
+	</a>
+	<a href="#">
+		<img src="http://puu.sh/nr5Z6/4360098fc1.png" /> SNTX <span>Ctrl + ?!</span>
+	</a>
+	<hr/>
+	<a href="feedback.php">
+		<i class="fas fa-comment"></i> Give Feedback
+	</a>
+	<a href="help.php">
+		<i class="fas fa-question-circle"></i> Font Awesome <span>Ctrl + ?!</span>
+	</a>
+</div>
+
+<script type="text/javascript">
+function goBack() {
+	window.history.back()
+}
+</script>
