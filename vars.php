a605591efe8e1de42e8407a6a98b272b5a9d312c
Joel Sprouse <joel.sprouse11@gmail.com>
2020-04-14 17:33:02 +0100

    Create vars.php

---
+++ vars.php
@@ -0,0 +1,85 @@
+<script type="text/javascript">
+var j1 = document.getElementById("jingle1");
+var j1play = document.getElementById("j1play");
+var j1stop = document.getElementById("j1stop");
+var j2 = document.getElementById("jingle2");
+var j2play = document.getElementById("j2play");
+var j2stop = document.getElementById("j2stop");
+var j3 = document.getElementById("jingle3");
+var j3play = document.getElementById("j3play");
+var j3stop = document.getElementById("j3stop");
+var j4 = document.getElementById("jingle4");
+var j4play = document.getElementById("j4play");
+var j4stop = document.getElementById("j4stop");
+var j5 = document.getElementById("jingle5");
+var j5play = document.getElementById("j5play");
+var j5stop = document.getElementById("j5stop");
+var j6 = document.getElementById("jingle6");
+var j6play = document.getElementById("j6play");
+var j6stop = document.getElementById("j6stop");
+var j7 = document.getElementById("jingle7");
+var j7play = document.getElementById("j7play");
+var j7stop = document.getElementById("j7stop");
+var j8 = document.getElementById("jingle8");
+var j8play = document.getElementById("j8play");
+var j8stop = document.getElementById("j8stop");
+var j9 = document.getElementById("jingle9");
+var j9play = document.getElementById("j9play");
+var j9stop = document.getElementById("j9stop");
+var j10 = document.getElementById("jingle10");
+var j10play = document.getElementById("j10play");
+var j10stop = document.getElementById("j10stop");
+var j11 = document.getElementById("jingle11");
+var j11play = document.getElementById("j11play");
+var j11stop = document.getElementById("j11stop");
+var j12 = document.getElementById("jingle12");
+var j12play = document.getElementById("j12play");
+var j12stop = document.getElementById("j12stop");
+var j13 = document.getElementById("jingle13");
+var j13play = document.getElementById("j13play");
+var j13stop = document.getElementById("j13stop");
+var j14 = document.getElementById("jingle14");
+var j14play = document.getElementById("j14play");
+var j14stop = document.getElementById("j14stop");
+
+
+var joel1 = document.getElementById("joeljingle1");
+var joel1play = document.getElementById("joel1play");
+var joel1stop = document.getElementById("joel1stop");
+var joel2 = document.getElementById("joeljingle2");
+var joel2play = document.getElementById("joel2play");
+var joel2stop = document.getElementById("joel2stop");
+var ozey1 = document.getElementById("ozeyjingle1");
+var ozey1play = document.getElementById("ozey1play");
+var ozey1stop = document.getElementById("ozey1stop");
+var ozey2 = document.getElementById("ozeyjingle2");
+var ozey2play = document.getElementById("ozey2play");
+var ozey2stop = document.getElementById("ozey2stop");
+var ozey3 = document.getElementById("ozeyjingle3");
+var ozey3play = document.getElementById("ozey3play");
+var ozey3stop = document.getElementById("ozey3stop");
+var loop1 = document.getElementById("loop1");
+var loop1play = document.getElementById("loop1play");
+var loop1stop = document.getElementById("loop1stop");
+var loop2 = document.getElementById("loop2");
+var loop2play = document.getElementById("loop2play");
+var loop2stop = document.getElementById("loop2stop");
+
+
+
+
+var DeckA = document.getElementById("DeckAAudio");
+var DeckB = document.getElementById("DeckBAudio");
+var DeckAPlay = document.getElementById("DeckAPlay");
+var DeckBPlay = document.getElementById("DeckBPlay");
+var DeckAPause = document.getElementById("DeckAPause");
+var DeckBPause = document.getElementById("DeckBPause");
+</script>
+
+<script type="text/javascript">
+var volANew = 1;
+var volBNew = 1;
+var DeckADurationF = "";
+var DeckACurrentF = "";
+var DeckARemainingF = "";
+</script>
