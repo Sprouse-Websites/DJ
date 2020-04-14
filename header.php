9a97acd410e3d4bf699cb41304aef0eef4c330d2
Joel Sprouse <joel.sprouse11@gmail.com>
2020-04-14 17:32:13 +0100

    Create header.php

---
+++ header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- Google Analytics -->
+	<!-- Global Site Tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-145359620-1"></script>
+	<script>
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
+	gtag('config', 'UA-145359620-1', { 'optimize_id': 'GTM-5W3HGPF'});
+</script>
+
+
+<meta charset="UTF-8">
+<!-- <meta http-equiv="refresh" content="5"> -->
+<!-- https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- <meta http-equiv="Content-Security-Policy" content="default-src 'self'; script-src 'self'">
+<meta http-equiv="X-Content-Security-Policy" content="default-src 'self'; script-src 'self'"> -->
+
+<!-- <link rel="stylesheet" href="http://www.w3schools.com/w3css/4/w3.css"> -->
+
+<title>DJ</title>
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="css/search.css">
+<link rel="stylesheet" href="css/rcm.css">
+<link rel="icon" href="media/icon.png">
+<script src="https://kit.fontawesome.com/e8abc56752.js" crossorigin="anonymous"></script>
+<script src="js/addtodecks.js" charset="utf-8"></script>
+<script src="jcontrols.js" charset="utf-8"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+</head>
+<body onload="load()">
