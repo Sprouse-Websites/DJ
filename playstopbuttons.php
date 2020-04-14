cee48ab079d15268ea080287750397f7654da67c
Joel Sprouse <joel.sprouse11@gmail.com>
2020-04-14 17:32:55 +0100

    js functions for audio

---
+++ playstopbuttons.php
@@ -0,0 +1,211 @@
+<script>
+// Play and Pause Buttons
+
+function playJoel1() {
+	joel1.play();
+	joel1play.style.display = "none";
+	joel1stop.style.display = "inline";
+}
+
+function stopJoel1() {
+	joel1.pause();
+	joel1.currentTime = 0;
+	joel1play.style.display = "inline";
+	joel1stop.style.display = "none";
+}
+
+function playJoel2() {
+	joel2.play();
+	joel2play.style.display = "none";
+	joel2stop.style.display = "inline";
+}
+
+function stopJoel2() {
+	joel2.pause();
+	joel2.currentTime = 0;
+	joel2play.style.display = "inline";
+	joel2stop.style.display = "none";
+}
+
+function playOzey1() {
+	ozey1.play();
+	ozey1play.style.display = "none";
+	ozey1stop.style.display = "inline";
+}
+
+function stopOzey1() {
+	ozey1.pause();
+	ozey1.currentTime = 0;
+	ozey1play.style.display = "inline";
+	ozey1stop.style.display = "none";
+}
+
+function playOzey2() {
+	ozey2.play();
+	ozey2play.style.display = "none";
+	ozey2stop.style.display = "inline";
+}
+
+function stopOzey2() {
+	ozey2.pause();
+	ozey2.currentTime = 0;
+	ozey2play.style.display = "inline";
+	ozey2stop.style.display = "none";
+}
+
+function playOzey3() {
+	ozey3.play();
+	ozey3play.style.display = "none";
+	ozey3stop.style.display = "inline";
+}
+
+function stopOzey3() {
+	ozey3.pause();
+	ozey3.currentTime = 0;
+	ozey3play.style.display = "inline";
+	ozey3stop.style.display = "none";
+}
+
+function playLoop1() {
+	loop1.play();
+	loop1play.style.display = "none";
+	loop1stop.style.display = "inline";
+}
+
+function stopLoop1() {
+	loop1.pause();
+	loop1.currentTime = 0;
+	loop1play.style.display = "inline";
+	loop1stop.style.display = "none";
+}
+
+function playLoop2() {
+	loop2.play();
+	loop2play.style.display = "none";
+	loop2stop.style.display = "inline";
+}
+
+function stopLoop2() {
+	loop2.pause();
+	loop2.currentTime = 0;
+	loop2play.style.display = "inline";
+	loop2stop.style.display = "none";
+}
+
+function playLoop3() {
+	loop3.play();
+	loop3play.style.display = "none";
+	loop3stop.style.display = "inline";
+}
+
+function stopLoop3() {
+	loop3.pause();
+	loop3.currentTime = 0;
+	loop3play.style.display = "inline";
+	loop3stop.style.display = "none";
+}
+
+function playSFX1() {
+	sfx1.play();
+	sfx1play.style.display = "none";
+	sfx1stop.style.display = "inline";
+}
+
+function stopSFX1() {
+	sfx1.pause();
+	sfx1.currentTime = 0;
+	sfx1play.style.display = "inline";
+	sfx1stop.style.display = "none";
+}
+
+function playSFX2() {
+	sfx2.play();
+	sfx2play.style.display = "none";
+	sfx2stop.style.display = "inline";
+}
+
+function stopSFX2() {
+	sfx2.pause();
+	sfx2.currentTime = 0;
+	sfx2play.style.display = "inline";
+	sfx2stop.style.display = "none";
+}
+
+function playSFX3() {
+	sfx3.play();
+	sfx3play.style.display = "none";
+	sfx3stop.style.display = "inline";
+}
+
+function stopSFX3() {
+	sfx3.pause();
+	sfx3.currentTime = 0;
+	sfx3play.style.display = "inline";
+	sfx3stop.style.display = "none";
+}
+
+function playSFX4() {
+	sfx4.play();
+	sfx4play.style.display = "none";
+	sfx4stop.style.display = "inline";
+}
+
+function stopSFX4() {
+	sfx4.pause();
+	sfx4.currentTime = 0;
+	sfx4play.style.display = "inline";
+	sfx4stop.style.display = "none";
+}
+
+function playSFX5() {
+	sfx5.play();
+	sfx5play.style.display = "none";
+	sfx5stop.style.display = "inline";
+}
+
+function stopSFX5() {
+	sfx5.pause();
+	sfx5.currentTime = 0;
+	sfx5play.style.display = "inline";
+	sfx5stop.style.display = "none";
+}
+
+function playSFX6() {
+	sfx6.play();
+	sfx6play.style.display = "none";
+	sfx6stop.style.display = "inline";
+}
+
+function stopSFX6() {
+	sfx6.pause();
+	sfx6.currentTime = 0;
+	sfx6play.style.display = "inline";
+	sfx6stop.style.display = "none";
+}
+
+function playSFX7() {
+	sfx7.play();
+	sfx7play.style.display = "none";
+	sfx7stop.style.display = "inline";
+}
+
+function stopSFX7() {
+	sfx7.pause();
+	sfx7.currentTime = 0;
+	sfx7play.style.display = "inline";
+	sfx7stop.style.display = "none";
+}
+
+function playSFX8() {
+	sfx8.play();
+	sfx8play.style.display = "none";
+	sfx8stop.style.display = "inline";
+}
+
+function stopSFX8() {
+	sfx8.pause();
+	sfx8.currentTime = 0;
+	sfx8play.style.display = "inline";
+	sfx8stop.style.display = "none";
+}
+</script>
---
+++ playstopfuns.php
@@ -0,0 +1,182 @@
+<script>
+function playJ1() {
+	j1.play();
+	j1play.style.display = "none";
+	j1stop.style.display = "inline";
+}
+
+function stopJ1() {
+	j1.pause();
+	j1.currentTime = 0;
+	j1play.style.display = "inline";
+	j1stop.style.display = "none";
+}
+
+function playJ2() {
+	j2.play();
+	j2play.style.display = "none";
+	j2stop.style.display = "inline";
+}
+
+function stopJ2() {
+	j2.pause();
+	j2.currentTime = 0;
+	j2play.style.display = "inline";
+	j2stop.style.display = "none";
+}
+
+function playJ3() {
+	j3.play();
+	j3play.style.display = "none";
+	j3stop.style.display = "inline";
+}
+
+function stopJ3() {
+	j3.pause();
+	j3.currentTime = 0;
+	j3play.style.display = "inline";
+	j3stop.style.display = "none";
+}
+function playJ4() {
+	j4.play();
+	j4play.style.display = "none";
+	j4stop.style.display = "inline";
+}
+
+function stopJ4() {
+	j4.pause();
+	j4.currentTime = 0;
+	j4play.style.display = "inline";
+	j4stop.style.display = "none";
+}
+
+function playJ5() {
+	j5.play();
+	j5play.style.display = "none";
+	j5stop.style.display = "inline";
+}
+
+function stopJ5() {
+	j5.pause();
+	j5.currentTime = 0;
+	j5play.style.display = "inline";
+	j5stop.style.display = "none";
+}
+
+function playJ6() {
+	j6.play();
+	j6play.style.display = "none";
+	j6stop.style.display = "inline";
+}
+
+function stopJ6() {
+	j6.pause();
+	j6.currentTime = 0;
+	j6play.style.display = "inline";
+	j6stop.style.display = "none";
+}
+
+function playJ7() {
+	j7.play();
+	j7play.style.display = "none";
+	j7stop.style.display = "inline";
+}
+
+function stopJ7() {
+	j7.pause();
+	j7.currentTime = 0;
+	j7play.style.display = "inline";
+	j7stop.style.display = "none";
+}
+
+function playJ8() {
+	j8.play();
+	j8play.style.display = "none";
+	j8stop.style.display = "inline";
+}
+
+function stopJ8() {
+	j8.pause();
+	j8.currentTime = 0;
+	j8play.style.display = "inline";
+	j8stop.style.display = "none";
+}
+
+function playJ9() {
+	j9.play();
+	j9play.style.display = "none";
+	j9stop.style.display = "inline";
+}
+
+function stopJ9() {
+	j9.pause();
+	j9.currentTime = 0;
+	j9play.style.display = "inline";
+	j9stop.style.display = "none";
+}
+
+function playJ10() {
+	j10.play();
+	j10play.style.display = "none";
+	j10stop.style.display = "inline";
+}
+
+function stopJ10() {
+	j10.pause();
+	j10.currentTime = 0;
+	j10play.style.display = "inline";
+	j10stop.style.display = "none";
+}
+
+function playJ11() {
+	j11.play();
+	j11play.style.display = "none";
+	j11stop.style.display = "inline";
+}
+
+function stopJ11() {
+	j11.pause();
+	j11.currentTime = 0;
+	j11play.style.display = "inline";
+	j11stop.style.display = "none";
+}
+
+function playJ12() {
+	j12.play();
+	j12play.style.display = "none";
+	j12stop.style.display = "inline";
+}
+
+function stopJ12() {
+	j12.pause();
+	j12.currentTime = 0;
+	j12play.style.display = "inline";
+	j12stop.style.display = "none";
+}
+
+function playJ13() {
+	j13.play();
+	j13play.style.display = "none";
+	j13stop.style.display = "inline";
+}
+
+function stopJ13() {
+	j13.pause();
+	j13.currentTime = 0;
+	j13play.style.display = "inline";
+	j13stop.style.display = "none";
+}
+
+function playJ14() {
+	j14.play();
+	j14play.style.display = "none";
+	j14stop.style.display = "inline";
+}
+
+function stopJ14() {
+	j14.pause();
+	j14.currentTime = 0;
+	j14play.style.display = "inline";
+	j14stop.style.display = "none";
+}
+</script>
