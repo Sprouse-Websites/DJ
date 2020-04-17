c655a0acc28fa852439000c78070cdf30e9f483b
Joel Sprouse <joel.sprouse11@gmail.com>
2020-04-17 14:56:33 +0100

    Create about.php

---
+++ about.php
@@ -0,0 +1,14 @@
+<?php
+include 'header.php';
+?>
+
+<table>
+	<tr>
+		<td>Version</td>
+		<td><?php echo $version ?></td>
+	</tr>
+	<tr>
+		<td>Open Source</td>
+		<td><a href="https://github.com/Sprouse-Websites/DJ">https://github.com/Sprouse-Websites/DJ</a></td>
+	</tr>
+</table>
