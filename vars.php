aa1f081c4997d7e86b0c445b74f0279ae04e80f7
Joel Sprouse <joel.sprouse11@gmail.com>
2020-05-07 15:18:52 +0100

    Update vars.php

--- vars.php
+++ vars.php
@@ -1,19 +1,3 @@
 <!-- Start of vars.php file -->
-<script type="text/javascript">
 
-var DeckA = document.getElementById("DeckAAudio");
-var DeckB = document.getElementById("DeckBAudio");
-var DeckAPlay = document.getElementById("DeckAPlay");
-var DeckBPlay = document.getElementById("DeckBPlay");
-var DeckAPause = document.getElementById("DeckAPause");
-var DeckBPause = document.getElementById("DeckBPause");
-</script>
-
-<script type="text/javascript">
-var volANew = 1;
-var volBNew = 1;
-var DeckADurationF = "";
-var DeckACurrentF = "";
-var DeckARemainingF = "";
-</script>
 <!-- End of vars.php file -->
