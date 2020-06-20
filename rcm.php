05711d103f14dad67c5ea46f1b5c166afd92d52e
Joel Sprouse <joel.sprouse11@gmail.com>
2020-06-20 11:00:22 +0100

    Update rcm.php
    added target blank and js

--- rcm.php
+++ rcm.php
@@ -9,14 +9,14 @@
 		<span>Ctrl + P</span>
 	</a>
 	<hr/>
-	<a href="feedback.php">
+	<a href="feedback.php" target="_blank">
 		<i class="fas fa-comment"></i> Give Feedback
 	</a>
-	<a href="about.php">
+	<a href="about.php" target="_blank">
 		<i class="fas fa-question-circle"></i> About
 		<!-- <span>Ctrl + ?!</span> -->
 	</a>
-	<a href="help.php">
+	<a href="help.php" target="_blank">
 		<i class="fas fa-info-circle"></i> Help
 		<!-- <span>Ctrl + ?!</span> -->
 	</a>
@@ -25,6 +25,44 @@
 <script type="text/javascript">
 function goBack() {
 	window.history.back()
+}
+</script>
+
+<script type="text/javascript">
+var i = document.getElementById("rcm").style;
+if (document.addEventListener) {
+	document.addEventListener('contextmenu', function(e) {
+		var posX = e.clientX;
+		var posY = e.clientY;
+		menu(posX, posY);
+		e.preventDefault();
+	}, false);
+	document.addEventListener('click', function(e) {
+		i.opacity = "0";
+		setTimeout(function() {
+			i.visibility = "hidden";
+		}, 501);
+	}, false);
+} else {
+	document.attachEvent('oncontextmenu', function(e) {
+		var posX = e.clientX;
+		var posY = e.clientY;
+		menu(posX, posY);
+		e.preventDefault();
+	});
+	document.attachEvent('onclick', function(e) {
+		i.opacity = "0";
+		setTimeout(function() {
+			i.visibility = "hidden";
+		}, 501);
+	});
+}
+
+function menu(x, y) {
+	i.top = y + "px";
+	i.left = x + "px";
+	i.visibility = "visible";
+	i.opacity = "1";
 }
 </script>
 <!-- End of rcm.php file -->
