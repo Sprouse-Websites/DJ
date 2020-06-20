204a3f56da1cdaa31e63e1a63d0770694b2f457e
Joel Sprouse <joel.sprouse11@gmail.com>
2020-06-20 11:01:27 +0100

    Update index.php
    Changed some php includes to php require
    Moved rcm js script to rcm.php

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <?php
-include 'header.php';
+$page_name = "DJ";
+require 'head.php';
+require 'header.php';
 include 'vars.php';
-include 'decks.php';
+require 'decks.php';
 ?>
 <style media="screen">
-	.weatherbox:hover {
-		border: 1pt grey solid;
-		border: 19px;
-	}
+.weatherbox:hover {
+	border: 1pt grey solid;
+	border: 19px;
+}
 </style>
 <a href="weather.php" class="hidden weatherbox" target="_blank">
 	<?php
@@ -15,50 +17,9 @@ include 'decks.php';
 	?>
 </a>
 <?php
-include 'playlist.php';
+require 'playlist.php';
 include 'jingles.php';
+include 'midi.php';
 include 'rcm.php';
-?>
-
-
-<script type="text/javascript">
-var i = document.getElementById("rcm").style;
-if (document.addEventListener) {
-	document.addEventListener('contextmenu', function(e) {
-		var posX = e.clientX;
-		var posY = e.clientY;
-		menu(posX, posY);
-		e.preventDefault();
-	}, false);
-	document.addEventListener('click', function(e) {
-		i.opacity = "0";
-		setTimeout(function() {
-			i.visibility = "hidden";
-		}, 501);
-	}, false);
-} else {
-	document.attachEvent('oncontextmenu', function(e) {
-		var posX = e.clientX;
-		var posY = e.clientY;
-		menu(posX, posY);
-		e.preventDefault();
-	});
-	document.attachEvent('onclick', function(e) {
-		i.opacity = "0";
-		setTimeout(function() {
-			i.visibility = "hidden";
-		}, 501);
-	});
-}
-
-function menu(x, y) {
-	i.top = y + "px";
-	i.left = x + "px";
-	i.visibility = "visible";
-	i.opacity = "1";
-}
-</script>
-
-<?php
-include 'footer.php';
+require 'footer.php';
 ?>
