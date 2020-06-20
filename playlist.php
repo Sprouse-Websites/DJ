c7bf2aafa0fc29133bc0d1572ef3c836b9126547
Joel Sprouse <joel.sprouse11@gmail.com>
2020-06-20 10:52:32 +0100

    Update playlist.php
    New Search Ability. New sort ability. Uses config.php database constants.

--- playlist.php
+++ playlist.php
@@ -28,63 +28,61 @@ th.sort {
 	}
 </script> -->
 <br>
-<div id="SongsLoading" style="overflow:auto; height:240pt; background-color:white; font-size:30pt; text-align:center; vertical-align:middle;">
-	Song List Loading
-</div>
+<!-- <div id="SongsLoading" style="overflow:auto; height:240pt; background-color:white; font-size:30pt; text-align:center; vertical-align:middle;">
+Song List Loading
+</div> -->
 <button type="button" name="button" onclick="SongFull()" class="PlaylistButton">View all songs</button>
-<button type="button" name="button" onclick="ShowPlaylist1()" class="PlaylistButton" disabled>View Playlist 1</button>
 <br>
-<script type="text/javascript">
-console.error("Hi");
-</script>
-<div id="AllSongs" style="overflow:auto; height:240pt; display:none;">
+<div id="AllSongs" style="overflow:auto; height:240pt;">
 	<table id='myTable' class='banded' height='300pt'>
 		<thead>
 			<tr>
 				<th>Add to Deck</th>
-				<th id='SongHeader' class='playlistTableHeader'>
-					<!-- <a href='javascript:sortTableBySong()'> -->
-					Song Name
-					<!-- </a> -->
+				<th id='IdHeader' class='playlistHeader'>
+					<button onclick='javascript:sortTableById()' class="playlistHeader">
+						Song Id
+					</button>
 				</th>
-				<th id='ArtistHeader' class='playlistTableHeader'>
-					<!-- <a href='javascript:sortTableByArtist()'> -->
-					Artists
-					<!-- </a> -->
+				<th id='SongHeader' class='playlistHeader'>
+					<button onclick='javascript:sortTableBySong()' class="playlistHeader">
+						Song Name
+					</button>
 				</th>
-				<th id='AlbumHeader' class='playlistTableHeader'>
-					<!-- <a href='javascript:sortTableByAlbum()'> -->
-					Album
-					<!-- </a> -->
+				<th id='ArtistHeader' class='playlistHeader'>
+					<button onclick='javascript:sortTableByArtist()' class="playlistHeader">
+						Artists
+					</button>
 				</th>
-				<th class='playlistTableHeader'>Length</th>
+				<th id='AlbumHeader' class='playlistHeader'>
+					<button onclick='javascript:sortTableByAlbum()' class="playlistHeader">
+						Album
+					</button>
+				</th>
+				<th class='playlistTableHeader'>
+					<button onclick='javascript:sortTableByLength()' class="playlistHeader">
+						Length
+					</button>
+				</th>
 				<th id='GenreHeader' class='playlistTableHeader'>
-					<!-- <a href='javascript:sortTableBy'> -->
-					Genre
-					<!-- </a> -->
+					<button onclick='javascript:sortTableByGenre()' class="playlistHeader">
+						Genre
+					</button>
 				</th>
 				<th>
 					Version
 				</th>
-				<th class='playlistTableHeader'>
-					<p title='E means Explicit R means unsuitable for Radio N means Not Explicit'>
+				<th id='ExplicitHeader' class='playlistTableHeader'>
+					<button onclick='javascript:sortTableByExplicit()' class="playlistHeader" title='E means Explicit R means unsuitable for Radio N means Not Explicit'>
 						Explicit
-					</p>
+					</button>
 				</th>
-				<th>
-					<i class="fas fa-download"></i>
-				</th>
+				<th></th>
 			</tr>
 		</thead>
 		<?php
 		function getSongs() {
-			$servername = "mysql5.clusterdb.net";
-			$username = "music-g0j-u-261256";
-			$password = "AtlAs45678";
-			$dbname = "music-g0j-u-261256";
 			// Create connection
-			$conn = new mysqli($servername, $username, $password, $dbname);
-			$link = mysqli_connect($servername, $username, $password, $dbname);
+			$link = mysqli_connect(servername, username, password, dbname);
 
 			// Check connection
 			if($link === false){
@@ -98,21 +96,27 @@ console.error("Hi");
 				if(mysqli_num_rows($result) > 0){
 					while($row = mysqli_fetch_array($result)){
 						echo "<tr>";
-						echo "<td><button type='button' class='DeckButton' onclick='AddToA".$row['id']."()'>A</button>
-						<button type='button' class='DeckButton' onclick='AddToB".$row['id']."()'>B</button></td>";
+						echo "<td><button type='button' class='DeckButton AddToAButton' onclick='AddToA".$row['id']."()'>A</button>
+						<button type='button' class='DeckButton AddToBButton' onclick='AddToB".$row['id']."()'>B</button></td>";
+						$idInt = (int)$row['id'];
+						echo "<td>" . $idInt . "</td>";
 						echo "<td>" . $row['Song'] . "</td>";
 						echo "<td>";
 						if ($row['MainArtist'] != NULL) {
 							echo $row['MainArtist'];
+							echo "<script>var Artists".$row['id']." = '".$row['MainArtist']."';</script>";
 						};
 						if ($row['OtherArtist1'] != NULL) {
 							echo "; " . $row['OtherArtist1'] . "; ";
+							echo "<script>var Artists".$row['id']." = '".$row['MainArtist']."; ". $row['OtherArtist1']."';</script>";
 						};
 						if ($row['OtherArtist2'] != NULL) {
 							echo $row['OtherArtist2'] . "; ";
+							echo "<script>var Artists".$row['id']." = '".$row['MainArtist']."; ". $row['OtherArtist1']."; ". $row['OtherArtist2']."';</script>";
 						};
 						if ($row['OtherArtist3'] != NULL) {
 							echo $row['OtherArtist3'] . "; ";
+							echo "<script>var Artists".$row['id']." = '".$row['MainArtist']."; ". $row['OtherArtist1']."; ". $row['OtherArtist2']."';</script>";
 						};
 						if ($row['OtherArtist4'] != NULL) {
 							echo $row['OtherArtist4'] . ";";
@@ -139,51 +143,94 @@ console.error("Hi");
 							};
 							echo $seconds;
 						}
+						else {
+							echo "<red>Unknown</red>";
+						}
 						echo "</td>";
-						echo "<td>" . $row['Genre'] . "</td>";
+						if ($row['Genre'] != NULL) {
+							echo "<td>" . $row['Genre'] . "</td>";
+						} else {
+							echo "<td><red>Unknown</red></td>";
+						}
 						echo "<td>" . $row['Version'] . "</td>";
-						echo "<td>" . $row['Explicit'] . "</td>";
-						$URL1 = 'http://sprousewebsites.co.uk/hosted-files/dj/songs/Music/'.$row['MainArtist']."/".$row['Album']."/".$row['Song'].".mp3";
-						$URL2 = 'http://sprousewebsites.co.uk/hosted-files/dj/songs/Music/'.$row['MainArtist']."/".$row['Album']."/01 ".$row['Song'].".mp3";
-						$sourcea = '<audio id="DeckAAudioEl"><source src="'.$URL1.'" type="audio/mpeg"><source src="'.$URL2.'" type="audio/mpeg">Your browser does not support audio elements</audio>';
-						$sourceb = '<audio id="DeckBAudioEl"><source src="'.$URL1.'" type="audio/mpeg"><source src="'.$URL2.'" type="audio/m4a">Your browser does not support audio elements</audio>';
-						echo "<td><a href='".$URL1."' class='hidden' download><i class='fas fa-arrow-alt-circle-down'></i></a></td>";
+						if ($row['Explicit'] == NULL) {
+							echo "<td><red>Unknown</red></td>";
+						}
+						elseif ($row['Explicit'] == "N") {
+							echo "<td><p title='Not Explicit'>" . $row['Explicit'] . "</p></td>";
+						}
+						elseif ($row['Explicit'] == "E") {
+							echo "<td><p title='Explicit'>" . $row['Explicit'] . "</p></td>";
+						}
+						elseif ($row['Explicit'] == "R") {
+							echo "<td><p title='Unsuitable for Radio'>" . $row['Explicit'] . "</p></td>";
+						}
+						elseif ($row['Explicit'] == "C") {
+							echo "<td><p title='Clean Version'>" . $row['Explicit'] . "</p></td>";
+						}
+						else {
+							echo "<td>" . $row['Explicit'] . "</td>";
+						};
+
+						$URL1 = 'https://sprousewebsitestest.com/dj/songs/'.$row['MainArtist']."/".$row['Album']."/".$row['Song'].".mp3";
+						$URL2 = 'https://sprousewebsitestest.com/dj/songs/Various Artists/'.$row['Album']."/".$row['Song'].".mp3";
+						$URL3 = 'https://sprousewebsitestest.com/dj/songs/'.$row['MainArtist']."/".$row['Album']."/01 ".$row['Song'].".mp3";
+						$URL4 = 'https://sprousewebsitestest.com/dj/songs/Compilations/'.$row['Album']."/".$row['Song'].".mp3";
+						$sourcea = '<audio id="DeckAAudioEl" oncanplay="DeckACanPlayThrough()" preload="auto"><source src="'.$URL1.'" type="audio/mpeg"><source src="'.$URL2.'" type="audio/mpeg"><source src="'.$URL3.'" type="audio/mpeg"><source src="'.$URL4.'" type="audio/mpeg">Your browser does not support audio elements</audio>';
+						$sourceb = '<audio id="DeckBAudioEl" oncanplay="DeckBCanPlay()" oncanplaythrough="DeckBCanPlayThrough()" preload="auto"><source src="'.$URL1.'" type="audio/mpeg"><source src="'.$URL2.'" type="audio/m4a"><source src="'.$URL3.'" type="audio/m4a"><source src="'.$URL4.'" type="audio/m4a">Your browser does not support audio elements</audio>';
+						echo "<td>";
+						echo "</td>";
 						echo "</tr>";
 						$varname = "URL".$row['id'];
 						$smarks = "'";
 						echo "<script>
 						function AddToA".$row['id']."() {
 							try {
+								DeckANotPlay();
 								document.getElementById('DeckAAudioDiv').innerHTML = ".$smarks.$sourcea.$smarks.";
+								document.getElementById('DeckAArtistText').innerHTML = Artists".$row['id'].";
+								document.getElementById('DeckASongText').innerHTML = '".$row['Song']."';
+								document.getElementById('DeckAAlbumText').innerHTML = '".$row['Album']."';
+								document.getElementById('DeckAPlay').disabled = false;
+								document.getElementById('DeckAEject').disabled = false;
+								document.getElementById('DeckAAudioEl').volume = volANew;
+								document.getElementById('DeckALengthRange').disabled = false;
+								document.getElementById('pictureA').src = 'https://sprousewebsitestest.com/dj/songs/".$row['MainArtist']."/".$row['Album']."/".$row['Album'].".jpg';
+								DeckAPause()
+								getDeckTimes();
+								document.getElementById('DeckAAudioEl').load();
+								DeckAPlay()
+								DeckAPause()
 							} catch (e) {
 								console.error(e.name);
 								console.error(e.message);
+								document.getElementById('snackbar').innerHTML = e.name + e.message + 'Unable to add song to Deck A';
+								snackbar();
 							}
-
-							document.getElementById('DeckAArtistText').innerHTML = '".$row['MainArtist'].$row['OtherArtist1'].$row['OtherArtist2'].$row['OtherArtist3'].$row['OtherArtist4']."';
-							document.getElementById('DeckASongText').innerHTML = '".$row['Song']."';
-							document.getElementById('DeckAAlbumText').innerHTML = '".$row['Album']."';
-							document.getElementById('DeckAPlay').disabled = false;
-							document.getElementById('DeckAEject').disabled = false;
-							document.getElementById('DeckAAudioEl').volume = volANew;
-							document.getElementById('DeckALengthRange').disabled = false;
-							document.getElementById('pictureA').src = 'http://sprousewebsites.co.uk/hosted-files/dj/songs/Music/".$row['MainArtist']."/".$row['Album']."/".$row['Album'].".jpg';
-							DeckAPause()
-							getDeckTimes();
 						}
 
 						function AddToB".$row['id']."() {
-							document.getElementById('DeckBAudioDiv').innerHTML = ".$smarks.$sourceb.$smarks.";
-							document.getElementById('DeckBArtistText').innerHTML = '".$row['MainArtist'].$row['OtherArtist1'].$row['OtherArtist2'].$row['OtherArtist3'].$row['OtherArtist4']."';
-							document.getElementById('DeckBSongText').innerHTML = '".$row['Song']."';
-							document.getElementById('DeckBAlbumText').innerHTML = '".$row['Album']."';
-							document.getElementById('DeckBPlay').disabled = false;
-							document.getElementById('DeckBEject').disabled = false;
-							document.getElementById('DeckBAudioEl').volume = volBNew;
-							document.getElementById('DeckBLengthRange').disabled = false;
-							document.getElementById('pictureB').src = 'http://sprousewebsites.co.uk/hosted-files/dj/songs/Music/".$row['MainArtist']."/".$row['Album']."/".$row['Album'].".jpg';
-							DeckBPause()
-							getDeckTimes();
+							try {
+								document.getElementById('DeckBAudioDiv').innerHTML = ".$smarks.$sourceb.$smarks.";
+								document.getElementById('DeckBArtistText').innerHTML = '".$row['MainArtist'].$row['OtherArtist1'].$row['OtherArtist2'].$row['OtherArtist3'].$row['OtherArtist4']."';
+								document.getElementById('DeckBSongText').innerHTML = '".$row['Song']."';
+								document.getElementById('DeckBAlbumText').innerHTML = '".$row['Album']."';
+								document.getElementById('DeckBPlay').disabled = false;
+								document.getElementById('DeckBEject').disabled = false;
+								document.getElementById('DeckBAudioEl').volume = volBNew;
+								document.getElementById('DeckBLengthRange').disabled = false;
+								document.getElementById('pictureB').src = 'https://sprousewebsitestest.com/dj/songs/".$row['MainArtist']."/".$row['Album']."/".$row['Album'].".jpg';
+								DeckBPause()
+								getDeckTimes();
+								document.getElementById('DeckBAudioEl').load();
+								DeckBPlay()
+								DeckBPause()
+							} catch (e) {
+								console.error(e.name);
+								console.error(e.message);
+								document.getElementById('ErrorSnackbar').innerHTML = e.name + ' ' + e.message + 'Unable to add song to Deck B';
+								ErrorSnackbar();
+							}
 						}
 						</script>
 						";
@@ -208,173 +255,16 @@ console.error("Hi");
 		if(isset($_POST['refresh'])) {
 			getSongs();
 		}
-		echo "<script type='text/javascript'>";
-		echo "var SongsInt = '".$SongsInt."';";
+		// echo "<script type='text/javascript'>";
+		// echo "var SongsInt = '".$SongsInt."';";
 		?>
-
+		<script type='text/javascript'>
 		console.log("Songs from database recieved");
-		document.getElementById("SongsLoading").style.display = "none";
-		document.getElementById("AllSongs").style.display = "block";
+		// document.getElementById("SongsLoading").style.display = "none";
+		// document.getElementById("AllSongs").style.display = "block";
 		document.getElementById("snackbar").innerHTML = "Songs from Database loaded";
 		snackbar();
-	</script>
-</div>
-<div id="Playlist1" class="DisplayNone">
-	<table id='myTable' class='banded' height='300pt'>
-		<thead>
-			<tr>
-				<th>Add to Deck</th>
-				<th id='SongHeader' class='playlistTableHeader'>
-					<!-- <a href='javascript:sortTableBySong()'> -->
-					Song Name
-					<!-- </a> -->
-				</th>
-				<th id='ArtistHeader' class='playlistTableHeader'>
-					<!-- <a href='javascript:sortTableByArtist()'> -->
-					Artists
-					<!-- </a> -->
-				</th>
-				<th id='AlbumHeader' class='playlistTableHeader'>
-					<!-- <a href='javascript:sortTableByAlbum()'> -->
-					Album
-					<!-- </a> -->
-				</th>
-				<th class='playlistTableHeader'>Length</th>
-				<th id='GenreHeader' class='playlistTableHeader'>
-					<!-- <a href='javascript:sortTableBy'> -->
-					Genre
-					<!-- </a> -->
-				</th>
-				<th>
-					Version
-				</th>
-				<th class='playlistTableHeader'>
-					<p title='E means Explicit R means unsuitable for Radio N means Not Explicit'>
-						Explicit
-					</p>
-				</th>
-			</tr>
-		</thead>
-		<?php
-		function getPlaylist1() {
-			$servername = "mysql5.clusterdb.net";
-			$username = "music-g0j-u-261256";
-			$password = "AtlAs45678";
-			$dbname = "music-g0j-u-261256";
-			// Create connection
-			$conn = new mysqli($servername, $username, $password, $dbname);
-			$link = mysqli_connect($servername, $username, $password, $dbname);
-
-			// Check connection
-			if($link === false){
-				die("ERROR: Could not connect. " . mysqli_connect_error());
-			}
-
-			// Attempt select query execution
-			$sql = "SELECT * FROM playlist1";
-
-			if($result = mysqli_query($link, $sql)){
-				if(mysqli_num_rows($result) > 0){
-					while($row = mysqli_fetch_array($result)){
-						echo "<tr>";
-						echo "<td><button type='button' class='DeckButton' onclick='AddToA".$row['id']."()'>A</button>
-						<button type='button' class='DeckButton' onclick='AddToB".$row['id']."()'>B</button></td>";
-						echo "<td>" . $row['Song'] . "</td>";
-						echo "<td>";
-						if ($row['MainArtist'] != NULL) {
-							echo $row['MainArtist'];
-						};
-						if ($row['OtherArtist1'] != NULL) {
-							echo "; " . $row['OtherArtist1'] . "; ";
-						};
-						if ($row['OtherArtist2'] != NULL) {
-							echo $row['OtherArtist2'] . "; ";
-						};
-						if ($row['OtherArtist3'] != NULL) {
-							echo $row['OtherArtist3'] . "; ";
-						};
-						if ($row['OtherArtist4'] != NULL) {
-							echo $row['OtherArtist4'] . ";";
-						};
-						echo "</td>";
-						// echo "<td>" . $row['MainArtist'] . " " . $row['OtherArtist1']." ".$row['OtherArtist2']." ".$row['OtherArtist3']." ".$row['OtherArtist4']."</td>";
-						echo "<td>" . $row['Album'] . "</td>";
-						echo "<td>";
-						if ($row['Length'] != NULL) {
-							$minutes = 0;
-							$seconds = 0;
-							$length = $row['Length'];
-							while ($length > 60) {
-								$minutes = $minutes + 1;
-								$length = $length - 60;
-							};
-							$seconds = $length;
-							if ($minutes < 10) {
-								echo "0";
-							};
-							echo $minutes . ":";
-							if ($seconds < 10) {
-								echo "0";
-							};
-							echo $seconds;
-						}
-						echo "</td>";
-						echo "<td>" . $row['Genre'] . "</td>";
-						echo "<td>" . $row['Version'] . "</td>";
-						echo "<td>" . $row['Explicit'] . "</td>";
-						echo "</tr>";
-						$varname = "URL".$row['id'];
-						$smarks = "'";
-						$URL1 = 'http://sprousewebsites.co.uk/hosted-files/dj/songs/Music/'.$row['MainArtist']."/".$row['Album']."/".$row['Song'].".mp3";
-						$URL2 = 'http://sprousewebsites.co.uk/hosted-files/dj/songs/Music/'.$row['MainArtist']."/".$row['Album']."/01 ".$row['Song'].".mp3";
-						$sourcea = '<audio id="DeckAAudioEl"><source src="'.$URL1.'" type="audio/mpeg"><source src="'.$URL2.'" type="audio/mpeg">Your browser does not support audio elements</audio>';
-						$sourceb = '<audio id="DeckBAudioEl"><source src="'.$URL1.'" type="audio/mpeg"><source src="'.$URL2.'" type="audio/m4a">Your browser does not support audio elements</audio>';
-						echo "<script>
-						function AddToA".$row['id']."() {
-							document.getElementById('DeckAAudioDiv').innerHTML = ".$smarks.$sourcea.$smarks.";
-							document.getElementById('DeckAArtistText').innerHTML = '".$row['MainArtist'].$row['OtherArtist1'].$row['OtherArtist2'].$row['OtherArtist3'].$row['OtherArtist4']."';
-							document.getElementById('DeckASongText').innerHTML = '".$row['Song']."';
-							document.getElementById('DeckAAlbumText').innerHTML = '".$row['Album']."';
-							document.getElementById('DeckAPlay').disabled = false;
-							document.getElementById('DeckAEject').disabled = false;
-							document.getElementById('DeckAAudioEl').volume = volANew;
-							document.getElementById('DeckALengthRange').disabled = false;
-							document.getElementById('pictureA').src = 'http://sprousewebsites.co.uk/hosted-files/dj/songs/Music/".$row['MainArtist']."/".$row['Album']."/".$row['Album'].".jpg';
-							getDeckTimes();
-						}
-
-						function AddToB".$row['id']."() {
-							document.getElementById('DeckBAudioDiv').innerHTML = ".$smarks.$sourceb.$smarks.";
-							document.getElementById('DeckBArtistText').innerHTML = '".$row['MainArtist'].$row['OtherArtist1'].$row['OtherArtist2'].$row['OtherArtist3'].$row['OtherArtist4']."';
-							document.getElementById('DeckBSongText').innerHTML = '".$row['Song']."';
-							document.getElementById('DeckBAlbumText').innerHTML = '".$row['Album']."';
-							document.getElementById('DeckBPlay').disabled = false;
-							document.getElementById('DeckBEject').disabled = false;
-							document.getElementById('DeckBAudioEl').volume = volBNew;
-							document.getElementById('DeckBLengthRange').disabled = false;
-							document.getElementById('pictureB').src = 'http://sprousewebsites.co.uk/hosted-files/dj/songs/Music/".$row['MainArtist']."/".$row['Album']."/".$row['Album'].".jpg';
-							getDeckTimes();
-						}
-						</script>
-						";
-					}
-					echo "</table>";
-					echo "<span id='songsfound'>". mysqli_num_rows($result)." Songs found in the Library</span>";
-					// Free result set
-					mysqli_free_result($result);
-				} else{
-					echo "<tr>";
-					echo "<td colspan='11'>No songs were found in this playlist.</td>";
-					echo "</tr>";
-					echo "</table>";
-				}
-			} else{
-				echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
-			}
-		}
-
-		getPlaylist1();
-		?>
+		</script>
 	</div>
 	<script type="text/javascript">
 	const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
