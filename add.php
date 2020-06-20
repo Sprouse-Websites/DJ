387d39aef6278741e651c90e4df80c6a9b13a95f
Joel Sprouse <joel.sprouse11@gmail.com>
2020-06-20 10:57:31 +0100

    Update add.php
    include head.php
    New form
    Autocomplete for some fields

--- add.php
+++ add.php
@@ -1,39 +1,91 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title></title>
-<link rel="icon" href="media/icon.png">
-</head>
+<?php
+include 'head.php';
+?>
+<style media="screen">
+body {
+	text-align: center;
+}
+input, textarea {
+	width: 100%;
+}
+form {
+	width: 40%;
+	margin: auto;
+	vertical-align: middle;
+	border: black 2pt solid;
+	border-radius: 30px;
+	padding: 30px;
+}
 
+i.required::before {
+	font-style: normal;
+	content: "*";
+}
+</style>
 <body>
 
-<form class="" action="db_add.php" method="POST">
-	Song name <input type="text" name="song" value="" required><br>
-	Main Artist <input type="text" name="artist" required><br>
-	Other Artist 1 <input type="text" name="artist1"><br>
-	Other Artist 2 <input type="text" name="artist2"><br>
-	Other Artist 3 <input type="text" name="artist3"><br>
-	Other Artist 4 <input type="text" name="artist4"><br>
-	Album <input type="text" name="album"><br>
-	Song Length <input type="text" name="Length" value=""><br>
-	Genre <input type="text" name="Genre" value=""><br>
-	Version <input type="text" name="version" value="Original"><br>
-	Remixer <input type="text" name="remixer" value=""><br>
-	Explicit <select class="" name="Explicit">
-		<option value="N" selected>Not Explicit</option>
-		<option value="R">Unsuitable for Radio</option>
-		<option value="C">Clean Version</option>
-		<option value="E">Explicit</option>
-	</select>
-	<br>
-	<!-- Released <input type="number" name="released"><br> -->
-	<!-- Date Bought <input type="date" name="bought"><br> -->
-	<!-- Price £<input type="text" name="currency-field" id="currency-field" pattern="^\£\d{1,3}(,\d{3})*(\.\d+)?£" value="" data-type="currency" value="0.99"><br> -->
-	Other Comments <textarea name="comments" maxlength="5235"></textarea><br>
-	<!-- <input type="file" name="songUpload"> -->
-	<input type="submit" name="submit" value="submit">
-</form>
+	<form class="" action="confirm.php" method="post">
+		Song name <i class="required"></i>
+		<br>
+		<input type="text" name="song" value="" required><br>
+		Main Artist <i class="required"></i>
+		<br>
+		<?php include 'autocomplete-artists.php'; ?>
+		<br>
+		Other Artist 1
+		<br>
+		<input type="text" name="artist1"><br>
+		Other Artist 2
+		<br>
+		<input type="text" name="artist2"><br>
+		Other Artist 3
+		<br>
+		<input type="text" name="artist3"><br>
+		Other Artist 4
+		<br>
+		<input type="text" name="artist4"><br>
+		Album
+		<br>
+		<?php include 'autocomplete-albums.php'; ?>
+		<br>
+		Song Length (in seconds)
+		<br>
+		<input type="text" name="length" value=""><br>
+		Genre <i class="required"></i>
+		<br>
+		<?php include 'autocomplete-genre.php'; ?>
+		<br>
+		Version <i class="required"></i>
+		<br>
+		<input type="text" name="version" required value="Original"><br>
+		Remixer
+		<br>
+		<input type="text" name="remixer" value=""><br>
+		Explicit <i class="required"></i>
+		<br>
+		<select class="" name="explicit" required>
+			<option value="" selected disabled></option>
+			<option value="N">Not Explicit</option>
+			<option value="R">Unsuitable for Radio</option>
+			<option value="C">Clean Version</option>
+			<option value="E">Explicit</option>
+		</select>
+		<br>
+		Year Released
+		<br>
+		<input type="number" name="yreleased"><br>
+		Date Released <br>
+		<input type="date" name="dreleased"><br>
+		Date Bought <br>
+		<input type="date" name="bought"><br>
+		<!-- Price £<input type="text" name="currency-field" id="currency-field" pattern="^\£\d{1,3}(,\d{3})*(\.\d+)?£" value="" data-type="currency" value="0.99"><br> -->
+		Other Comments
+		<br>
+		<textarea name="comments" maxlength="5235"></textarea><br>
+		<!-- <input type="file" name="songUpload"> -->
+		<input type="submit" name="submit" value="submit">
+	</form>
 
-<script src="js/currency-input.js"></script>
+	<script src="js/currency-input.js"></script>
 </body>
 </html>
