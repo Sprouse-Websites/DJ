3988c3447959f55684e8495f565bc314c93772b8
Joel Sprouse <joel.sprouse11@gmail.com>
2020-06-20 10:57:42 +0100

    Update about.php

--- about.php
+++ about.php
@@ -1,39 +1,80 @@
 <?php
-include 'header.php';
+$page_name = "About DJ";
+include 'head.php';
 ?>
-<link rel="stylesheet" href="css/calendar.css">
+<link rel="stylesheet" href="https://www.sprousewebsitestest.com/dj/css/calendar.css">
 
 <table>
 	<tr>
 		<td>Version</td>
-		<td><?php echo $version ?></td>
+		<td><?php echo version; ?></td>
 	</tr>
 	<tr>
 		<td>Open Source</td>
 		<td><a href="https://github.com/Sprouse-Websites/DJ">https://github.com/Sprouse-Websites/DJ</a></td>
 	</tr>
 </table>
+<div class="">
+	The Aim:
+	This project is software that will allow Radio Presenters to play music on the radio show.
+
+	Planned features include (but not limited to):
+	<ul>
+		<li>Search for songs</li>
+		<li>Add songs to a playlist</li>
+		<li>Volume Control</li>
+	</ul>
+
+</div>
+
+
 <div class="calendar-timeline">
 	<section id="cd-timeline" class="cd-container">
 		<div class="cd-timeline-block">
 			<div class="cd-timeline-img cd-bg-1 bounce-in">
 				<img src="/libs/vertical-timeline/img/cd-icon-calendar.svg" alt="Picture" data-pagespeed-url-hash="3250173501" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
-			</div> // cd-timeline-img
+			</div>
 			<div class="cd-timeline-content bounce-in">
-				<h2>Beta 0.1.8</h2>
+				<h2>Alpha 0.1.10 (Current)</h2>
+				<a href="https://web.archive.org/web/20200502101633/https://www.sprousewebsitestest.com/dj/">View archive</a>
+				<ul>
+					<li>Decks Revamp in progress</li>
+				</ul>
+			</div>
+		</div>
+		<div class="cd-timeline-block">
+			<div class="cd-timeline-img cd-bg-1 bounce-in">
+				<img src="/libs/vertical-timeline/img/cd-icon-calendar.svg" alt="Picture" data-pagespeed-url-hash="3250173501" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
+			</div>
+			<div class="cd-timeline-content bounce-in">
+				<h2>Alpha 0.1.9</h2>
+				<ul>
+					<li>About and Help pages open in a new tab to prevent audio stopping</li>
+				</ul>
+			</div>
+		</div>
+
+		<div class="cd-timeline-block">
+			<div class="cd-timeline-img cd-bg-1 bounce-in">
+				<img src="/libs/vertical-timeline/img/cd-icon-calendar.svg" alt="Picture" data-pagespeed-url-hash="3250173501" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
+			</div>
+			<div class="cd-timeline-content bounce-in">
+				<h2>Alpha 0.1.8</h2>
 				<ul>
 					<li>When adding a new song to decks, play button automatically shows.</li>
 					<li>Progress bar is less jittery</li>
 				</ul>
-			</div> // cd-timeline-content
-		</div> // cd-timeline-block
+			</div>
+		</div>
 
 		<div class="cd-timeline-block">
 			<div class="cd-timeline-img cd-bg-0 bounce-in">
 				<img src="/libs/vertical-timeline/img/cd-icon-calendar.svg" alt="Picture" data-pagespeed-url-hash="3250173501" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
 			</div> <!-- cd-timeline-img -->
 			<div class="cd-timeline-content bounce-in">
-				<h2>Beta 0.1.7</h2>
+				<h2>Alpha 0.1.7</h2>
+				<a href="https://web.archive.org/web/20200427205950/http://www.sprousewebsites.co.uk/hosted-files/dj/">View archive</a>
+
 				The first properly logged version. It featured:
 				<ul>
 					<li>Basic ability to play and pause songs</li>
